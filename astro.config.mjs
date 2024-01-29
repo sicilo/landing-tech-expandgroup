ac202e06a258c7eca223b506c8021bf261fcbf6a
sicilo <mejiamonsalve.camilo@gmail.com>
2024-01-28 23:49:10 -0500

    feat: adding branches and preparing github-actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://sicilo.pages.github.io',
+  base:'/landing-tech-expandgroup',
   integrations: [tailwind(), react({
     include: ['**/React/*']
   })]
